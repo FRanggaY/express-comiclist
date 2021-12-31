a79bf9483582f364a538828025605bb9a094c136
FRanggaY <franciscusrangga@gmail.com>
2021-12-31 10:18:43 +0700

    update app

--- app.js
+++ app.js
@@ -3,14 +3,33 @@
 // Created : 31 December 2021
 // Updated : 31 December 2021
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const comic = require('./routes/comic')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-//routes
-app.get('/testing', (req, res) => {
-    res.send('REST API')
-})
+const PORT = process.env.PORT || 8000
 
-const port = 8000
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(PORT, console.log(`Server runs at ${PORT}...`))
+    }catch(err){
+        console.log(err)
+    }
+}
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+//middleware
+// app.use(express.static('./public'))
+app.use(express.json())
+
+
+app.use('/api/v1/comic', comic)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+start()
